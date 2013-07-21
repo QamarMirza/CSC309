4d9c587209bf9946b30f626af5b94786dd4d84b5
shichu.lin <shichu.lin@gmail.com>
2013-07-21 00:01:09 +0000

    initial checking of airuoft (a2)

---
+++ airuoft/application/controllers/info.php
@@ -0,0 +1,41 @@
+<?php
+
+class Info extends CI_Controller {
+    
+    function __construct() {
+    	// Call the Controller constructor
+    	parent::__construct();
+    }
+
+    function setFirstName($first){
+
+
+    }
+
+    function setLastName($last){
+
+
+
+    }
+
+    function setCreditCard($CreditCardnum){
+
+
+
+    }
+
+    function setCreditCardExpir(CreditCardExpiry){
+
+
+    }
+
+    function register(){
+
+
+
+
+    }
+
+}
+
+?>
