60feffab55189421e052061644ea2a96ac7b0cba
shichu.lin@gmail.com <shichu.lin@gmail.com@847e44bd-d7d1-4fa1-8ab8-6a9f55462528>
2013-07-25 01:42:28 +0000

    got flights by date

--- airuoft/application/controllers/main.php
+++ airuoft/application/controllers/main.php
@@ -2,16 +2,16 @@
 
 class Main extends CI_Controller {
 
-    function __construct() {
-    	// Call the Controller constructor
-    	parent::__construct();
-    }
-        
-    function index() {
-	    	$data['main']='main/index';
-	    	$this->load->view('template', $data);
-    }
-    
+	function __construct() {
+		// Call the Controller constructor
+		parent::__construct();
+	}
+
+	function index() {
+		$data['main']='main/index';
+		$this->load->view('template', $data);
+	}
+
 	function showFlights() {
 		//First we load the library and the model
 		$this->load->library('table');
@@ -23,10 +23,10 @@ class Main extends CI_Controller {
 			//Prepare the array that will contain the data
 			$table = array();	
 			$table[] = array('From','To','Time','Date','Available');
-		   foreach ($flights->result() as $row){
+			foreach ($flights->result() as $row){
 				//This time we are not only adding a new link, but, in the third parameter of the anchor function we are adding an onclick behaviour to ask the user if he/she really wants to delete the record.
 				$table[] = array($row->from,$row->to,$row->time,$row->date,$row->available);
-		   }
+			}
 			//Next step is to place our created array into a new array variable, one that we are sending to the view.
 			$data['flights'] = $table; 		   
 		}
@@ -34,63 +34,59 @@ class Main extends CI_Controller {
 		$data['main']='main/flights';
 		$this->load->view('template', $data);
     }
-    
 
+	function populate() {
+		$this->load->model('flight_model');
+		$this->flight_model->populate();
+		
+		//Then we redirect to the index page again
+		redirect('', 'refresh');
+	}
 
-    function populate() {
-	    $this->load->model('flight_model');
-	    $this->flight_model->populate();
-	    
-	    //Then we redirect to the index page again
-	    redirect('', 'refresh');
-    }
-    
-    function delete() {
-    	$this->load->model('flight_model');    	 
-    	$this->flight_model->delete();
-    	 
-    	//Then we redirect to the index page again
-    	redirect('', 'refresh');  
-    }
+	function delete() {
+		$this->load->model('flight_model');    	 
+		$this->flight_model->delete();
+		 
+		//Then we redirect to the index page again
+		redirect('', 'refresh');  
+	}
  	
  	function addFlight() {
-    	$data['main'] = 'main/addFlight'; // calls controller
-    	$this->load->library('form_validation');
-    	$this->load->view('template', $data);
-    }
+		$data['main'] = 'main/addFlight'; // set the main view to addFlight.php
+		$this->load->library('form_validation');
+		$this->load->view('template', $data);
+	}
 /* ---------------------------------------------------------------------------------------- */
  
-    function availableFlights(){
-    	$this->load->Library('table');
-    	$this->load->model('flight_model');
-    	$date = $_REQUEST['date'];
-    	$campus = $_REQUEST['campus'];
-	    date_default_timezone_set("america/toronto");
-  		$date = date_create($date);
-  		$date = date_format($date, "Y-m-d");
-  		//$date = (string)$date;
-    	$flights = $this->flight_model->availableFlights($date, $campus);
-
+	function availableFlights(){
+		$this->load->Library('table');
+		$this->load->model('flight_model');
+		
+		$campus = $_REQUEST['campus'];	
+		date_default_timezone_set("UTC");
+		$date = $_REQUEST['date'];
+		
+		$flights = $this->flight_model->getAvailableFlights($date, $campus);
 		if ($flights->num_rows() > 0){
 			//Prepare the array that will contain the data
 			$table = array();	
 			$table[] = array('Date');
-		   foreach ($flights->result() as $row){
+			foreach ($flights->result() as $row){
 				//This time we are not only adding a new link, but, in the third parameter of the anchor function we are adding an onclick behaviour to ask the user if he/she really wants to delete the record.
 				$table[] = array($row->date);
-		   }
-			//Next step is to place our created array into a new array variable, one that we are sending to the view.
-			$data['flights'] = $table; 		   
+			}
+		//Next step is to place our created array into a new array variable, one that we are sending to the view.
+		$data['flights'] = $table;
 		}
 		//Now we are prepared to call the view, passing all the necessary variables inside the $data array
 		$data['main']='main/flights';
 		$this->load->view('template', $data);
-    }
-   
-    // USER ADDS THEIR INFORMATION
-    function addUser() {
-    	$data['main'] = 'main/addUser';
-    	$this->load->library('form_validation');
-    	$this->load->view('template', $data);
+	}
+
+	// USER ADDS THEIR INFORMATION
+	function addUser() {
+		$data['main'] = 'main/addUser';
+ 		$this->load->library('form_validation');
+		$this->load->view('template', $data);
     }
 }
