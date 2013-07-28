84ebce778ca533721e9189460d5d372e0ba8e808
shichu.lin@gmail.com <shichu.lin@gmail.com@847e44bd-d7d1-4fa1-8ab8-6a9f55462528>
2013-07-28 23:59:02 +0000

    ticket model

---
+++ airuoft/application/models/ticket_model.php
@@ -0,0 +1,24 @@
+<?php
+
+class ticket_model extends CI_Model {
+    function availableSeats($flightid){
+        $query = $this->db->query("select seat
+                                   from ticket t
+                                   where t.flight_id = '$flightid' ;");
+    
+        return $query;
+    }
+
+    function get_ticket(){
+        $query = $this->db->query(" select *
+                                    from ticket ;");
+        return $query;
+    }
+
+    function insertTicket($first, $last, $creditcard, $creditcardexpr, $flight_id, $seat) {
+        $this->db->query("insert into ticket (first, last, creditcardnumber, creditcardexpiration, flight_id, seat) 
+                          values ('$first', '$last', '$creditcard', '$creditcardexpr', $flight_id, $seat)");
+    }
+}
+
+?>
