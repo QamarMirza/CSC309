d0127f0bda403815c34313c6f8978f3b46d34379
shichu.lin <shichu.lin@gmail.com>
2013-07-29 00:22:23 +0000

    stuff works again

--- airuoft/application/views/main/addFlight.php
+++ airuoft/application/views/main/addFlight.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-
 	<h1>Select a Flight</h1>
-	<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
 	<link href="<?php echo base_url();?>css/addFlight.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
 	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 	<script type='text/javascript'>
 		$(function() {
 			$("#datepicker").datepicker({ minDate: +1, maxDate: "+14D" });
