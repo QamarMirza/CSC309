66f5a25d085e28008cd18fe025447ace3eb9f5dd
shichu.lin@gmail.com <shichu.lin@gmail.com@847e44bd-d7d1-4fa1-8ab8-6a9f55462528>
2013-07-29 01:08:48 +0000

    updates

--- airuoft/application/views/main/ticketUser.php
+++ airuoft/application/views/main/ticketUser.php
@@ -13,20 +13,20 @@
 		echo form_open('main/buyTicket');
 
 		echo form_label('First Name:'); 
-		echo form_error('FirstName');
-		echo form_input('FirstName', set_value('setFirstName'), "required | pattern='[a-zA-Z]' | max_length[16] | oninput='checkfirstName()' id='first' ");
+		echo form_error('firstName');
+		echo form_input('firstName', set_value('firstName'), "required | pattern='[a-zA-Z]' | max_length[16] | oninput='checkfirstName()' id='first' ");
 		
 		echo form_label('Last Name:'); 
-		echo form_error('LastName');
-		echo form_input('LastName', set_value('setLastName'),  "required | pattern='[a-zA-Z]' | max_length[16] id='last' oninput='checklastName()' " );
+		echo form_error('lastName');
+		echo form_input('lastName', set_value('lastName'),  "required | pattern='[a-zA-Z]' | max_length[16] id='last' oninput='checklastName()' " );
 		
 		echo form_label('Credit Card:'); 
-		echo form_error('CreditCard');
-		echo form_input('CreditCard', set_value('setCreditCardNumber'), "required pattern='\d{16}' id='creditcardnumber' oninput='checkNum()' ");
+		echo form_error('creditCard');
+		echo form_input('creditCard', set_value('creditCard'), "required pattern='\d{16}' id='creditcardnumber' oninput='checkNum()' ");
 		
 		echo form_label('Credit Card Expiration Date (MMYY)');
-		echo form_input('CreditCardExpr', set_value('setCreditCardExpir'), "required pattern='\d{4}' oninput='checkValid()' id ='expiry'"); 
-		echo form_error('CreditCardExpr');
+		echo form_input('creditCardExpr', set_value('creditCardExpr'), "required pattern='\d{4}' oninput='checkValid()' id ='expiry'"); 
+		echo form_error('creditCardExpr');
 
 		echo "<br />";
 		
