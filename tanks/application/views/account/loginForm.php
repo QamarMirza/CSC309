3115aa3f40fd187a1e0c4c0b1c76004710e624df
shichu.lin@gmail.com <shichu.lin@gmail.com@847e44bd-d7d1-4fa1-8ab8-6a9f55462528>
2013-08-05 03:26:55 +0000

    battle.js

--- tanks/application/views/account/loginForm.php
+++ tanks/application/views/account/loginForm.php
@@ -47,30 +47,25 @@
 	<h1>Login</h1>
 
     <?php 
-	    if (isset($errorMsg)) {
-		    echo "<p>" . $errorMsg . "</p>";
-	    }
-	    echo form_open('account/login');
-	    echo form_label('Username');
-	    echo form_error('username');
-	    echo form_input('username',set_value('username'),"required");
-	    echo form_label('Password'); 
-	    echo form_error('password');
-	    echo form_password('password','',"required");
-		echo form_submit('submit', 'Login', 'onClick=" return checktime();"');
-	    
-	    echo form_hidden('remaining', set_value('remaining'));
-	    
-	    echo "<p>" . anchor('account/newForm','Create Account') . "</p>";
-	    echo "<p>" . anchor('account/recoverPasswordForm','Recover Password') . "</p>";
+    if (isset($errorMsg)) {
+	    echo "<p>" . $errorMsg . "</p>";
+    }
+    echo form_open('account/login');
+    echo form_label('Username');
+    echo form_error('username');
+    echo form_input('username',set_value('username'),"required");
+    echo form_label('Password'); 
+    echo form_error('password');
+    echo form_password('password','',"required");
+	echo form_submit('submit', 'Login', 'onClick="return checktime();"');
+    
+    echo form_hidden('remaining', set_value('remaining'));
+    
+    echo "<p>" . anchor('account/newForm','Create Account') . "</p>";
+    echo "<p>" . anchor('account/recoverPasswordForm','Recover Password') . "</p>";
+    echo form_close();
     ?>
 
     <canvas id="drawingCanvas" width="200" height="200"></canvas>
-
-	<?php
-		//$onclick = array('onclick' => "document.getElementById('captcha').src = '/tanks/securimage/securimage_show.php?' + Math.random(); return false;");
-		//echo anchor('', "[ Different Image ]", $onclick);
-			echo form_close();
-	?>	
 </body>
 </html>
---
+++ tanks/js/arcade/battle.js
@@ -0,0 +1,274 @@
+var players = 2;
+var player1;
+var player2;
+var offsetX, offsetY; // used for moving tanks
+var mx = 0;
+var my = 0;
+
+$(function() {
+	// setup canvas
+	canvas = $("#drawingCanvas"); // this returns a jQuery object
+	canvasElement = canvas[0]; // canvas[0] is the actual HTML DOM element for our drawing canvas
+	context = canvasElement.getContext("2d");
+
+	// attach mouse events to entire document
+	$(window).keydown(function(event) {
+		keydownHandler(event);
+	});
+
+	drawTanks();
+});
+
+function keydownHandler(event){
+    //console.log('keycode: '+ event.keyCode);
+    if (event.keyCode === 37) { // left
+        mx = -2;
+        my = 0;
+    } else if (event.keyCode === 38) { // up
+        mx = 0;
+        my = -2;
+    } else if (event.keyCode === 39) { // right
+        mx = 2;
+        my = 0;
+    } else if (event.keyCode === 40) { // down
+        mx = 0;
+        my = 2;
+    }
+};
+
+/*
+	when a tank that is selected and is clicked on and drag we call this function
+	to update where it is moved to by calculating the offset from the mouse	
+*/
+function moveTank(x, y) {
+	var tank = previousSelectedTank;
+	if (tank instanceof Circle || tank instanceof Rectangle) {
+		tank.x1 = x - offsetX;
+		tank.y1 = y - offsetY;
+	} else {
+		var diffX = tank.x2 - tank.x1;
+		var diffY = tank.y2 - tank.y1;ss
+		tank.x1 = x - offsetX;
+		tank.y1 = y - offsetY;
+		tank.x2 = tank.x1 + diffX;
+		tank.y2 = tank.y1 + diffY;
+	}
+	drawTanks();		
+}
+
+function drawTanks() {
+	// Clear the canvasElement.
+	context.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+	// draw tank positioned for player number
+	player1 = new Tank(0, 150, 50, 200);
+    player1.tankBody.fillColor = "beige";
+    player1.tankBody.strokeColor = "blue";    
+	player1.draw();
+	
+	player2 = new Tank(150, 0, 200, 50);
+    player2.tankBody.fillColor = "beige";
+    player2.tankBody.strokeColor = "green";
+	player2.draw();
+}
+
+function clearCanvas(event) {
+	// Remove all the tanks.
+	tanks = [];
+
+	// Update the display.
+	drawTanks();
+}
+
+/*
+	This is the object created for the rectangle tank. we define it's point of origin and then offset values
+	as well as it's initial colour outline and width and fill colour.
+*/
+function Tank(x1, y1, x2, y2) {
+    this.tankBody = new TankBody(x1, y1, x2, y2);
+    var centerX = x1 + (x2 - x1) / 2;
+    var centerY = y1 + (y2 - y1) / 2;
+    this.turret = new Turret(centerX-8, centerY-8, x2/1.5, y2/1.5);
+    //this.cannon = new Cannon(centerX, centerY, x2, y2);
+    this.draw = function(){
+        this.tankBody.draw();
+        this.turret.draw();
+        //this.cannon.draw();        
+    }
+}
+
+/*
+	This is the object created for the tank body. 
+	We define it's point of origin and then offset values with keyboard
+*/
+function TankBody(x1, y1, x2, y2) {
+    this.update(x1, y1, x2, y2);
+	this.outlineWidth = 3;
+	this.draw = function() {
+		// Draw the rectangle.
+		context.globalAlpha = 0.85;
+		context.beginPath();
+		context.rect(this.x1, this.y1, this.x2, this.y2);
+		context.fillStyle = this.fillColor;
+		context.strokeStyle = this.strokeColor;
+		context.fill();
+		context.stroke();
+	};
+}
+
+TankBody.prototype.testHit = function(x, y) {
+	var precision;
+	if (this.outlineWidth <= 5) {
+		var precision = this.outlineWidth + 2;
+	} else if (this.outlineWidth <= 10) {
+		var precision = this.outlineWidth / 1.15;
+	} else if  (this.outlineWidth <= 15) {
+		var precision = this.outlineWidth / 1.5;
+	} else{
+		var precision = this.outlineWidth /1.8;
+	} 
+
+	if (this.x2 < 0) {
+		if (this.y2 < 0) { // if box is drawn bottom right to top left
+			if (this.y2 < 0) { 
+				if (this.x1  + precision> x && this.x1+this.x2 - precision < x && this.y1 + precision > y && this.y1+this.y2 - precision < y) { 
+					return true;
+				}
+				return false;
+			}
+		} else { // x >0, box is drawn top right to bottom left
+			if (this.y2 > 0) { 
+				if (this.x1 +precision > x && this.x1+this.x2  - precision< x && this.y1  - precision< y && this.y1+this.y2 + precision > y) {
+					return true;
+				}
+				return false;
+			}
+		}
+	} else { // x > 0
+		if (this.y2 < 0) { // box is drawn bottom left to top right
+			if (this.x1 - precision< x && this.x1+this.x2 +precision> x && this.y1  + precision> y && this.y1+this.y2 - precision< y) {
+				return true;
+			}
+			return false;
+		} else { // y > 0, box is drawn top left to bottom right 
+			if (this.x1 - precision < x && this.x1+this.x2  + precision> x && this.y1 - precision < y && this.y1+this.y2 + precision > y) {
+				return true;
+			}
+			return false;
+		}
+	}
+};
+
+TankBody.prototype.update = function (x1, y1, x2, y2) {
+	this.x1 = x1;
+	this.y1 = y1;
+	this.x2 = x2-this.x1; // offset value. distance between where tank begins and where mouse is
+	this.y2 = y2-this.y1;
+};
+
+	
+/*
+	This is the turret object created for the tank. 
+	we define it's point of origin and then offset values
+*/
+function Turret(x1, y1, x2, y2) {
+	this.update(x1, y1, x2, y2);
+	this.outlineWidth = 2
+	this.isSelected = false;
+	this.draw = function() {
+		// Draw the rectangle
+		context.globalAlpha = 0.85;
+		context.beginPath();
+		context.rect(this.x1, this.y1, this.x2, this.y2);
+		context.fillStyle = this.fillColor;
+		context.strokeStyle = this.strokeColor;
+		context.fill();
+		context.stroke();
+	};
+}
+
+Turret.prototype.testHit = function(x,y) {
+	var precision;
+		if (this.outlineWidth <= 5) {
+		var precision = this.outlineWidth + 2;
+	} else if (this.outlineWidth <= 10) {
+		var precision = this.outlineWidth / 1.15;
+	} else if  (this.outlineWidth <= 15) {
+		var precision = this.outlineWidth / 1.5;
+	} else{
+		var precision = this.outlineWidth /1.8;
+	} 
+
+	if (this.x2 < 0) {
+		if (this.y2 < 0) { // if box is drawn bottom right to top left
+			if (this.y2 < 0) { 
+				if (this.x1  + precision> x && this.x1+this.x2 - precision < x && this.y1 + precision > y && this.y1+this.y2 - precision < y) { 
+					return true;
+				}
+				return false;
+			}
+		} else { // x >0, box is drawn top right to bottom left
+			if (this.y2 > 0) { 
+				if (this.x1 +precision > x && this.x1+this.x2  - precision< x && this.y1  - precision< y && this.y1+this.y2 + precision > y) {
+					return true;
+				}
+				return false;
+			}
+		}
+	} else { // x > 0
+		if (this.y2 < 0) { // box is drawn bottom left to top right
+			if (this.x1 - precision< x && this.x1+this.x2 +precision> x && this.y1  + precision> y && this.y1+this.y2 - precision< y) {
+				return true;
+			}
+			return false;
+		} else { // y > 0, box is drawn top left to bottom right 
+			if (this.x1 - precision < x && this.x1+this.x2  + precision> x && this.y1 - precision < y && this.y1+this.y2 + precision > y) {
+				return true;
+			}
+			return false;
+		}
+	}
+};
+
+Turret.prototype.update = function (x1, y1, x2, y2) {
+	this.x1 = x1;
+	this.y1 = y1;
+	this.x2 = x2-this.x1; // offset value. distance between where tank begins and where mouse is
+	this.y2 = y2-this.y1;
+};
+
+function Cannon(x1, y1, x2, y2) {
+	this.update(x1, y1, x2, y2);
+	this.outlineWidth = 2
+	this.draw = function() {
+		// Draw the circle.
+		context.globalAlpha = 0.85;
+		context.beginPath();
+		context.arc(this.x1, this.y1, this.radius, 0, Math.PI*2);
+		context.fillStyle = "black";
+		context.strokeStyle = "black";
+		context.fill();
+		context.stroke(); 
+	};
+}
+
+Cannon.prototype.testHit = function(x, y) {
+	if (this.outlineWidth <= 5) {
+		var precision = this.outlineWidth;
+	} else if (this.outlineWidth <= 15) {
+		var precision = this.outlineWidth / 1.25;
+	} else {
+		var precision = this.outlineWidth / 1.75;
+	}
+	if (Math.sqrt(Math.pow(this.x1 - x, 2) + Math.pow(this.y1 -y, 2)) <= this.radius + precision) {
+		return true;
+	}
+	return false;
+};
+
+Cannon.prototype.update = function(x1, y1, x2, y2) {
+	this.x1 = x1;
+	this.y1 = y1;
+	this.radius = Math.sqrt(Math.pow(x2 - this.x1, 0.75) + Math.pow(y2-this.y1, 0.75));
+};
+
