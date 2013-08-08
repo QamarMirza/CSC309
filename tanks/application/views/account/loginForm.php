1935b44c8d386a024440df0f1c20ffe3ec318cca
qamar.mirza91 <qamar.mirza91@847e44bd-d7d1-4fa1-8ab8-6a9f55462528>
2013-08-08 22:35:49 +0000

    adsfsdfksdfksdfkjsdfdsflj

--- tanks/application/views/account/loginForm.php
+++ tanks/application/views/account/loginForm.php
@@ -1,71 +1,39 @@
 <!DOCTYPE html>
 
 <html>
-<head>
-	<style>
-		input {
-			display: block;
-		}
-		canvas {
-            border: 2px solid;
+    <head>
+    	<style>
+    		input {
+    			display: block;
+    		}
+    		canvas {
+                border: 2px solid;
+            }
+    	</style>
+    	<script src="http://code.jquery.com/jquery-latest.js"></script> 
+    	<script type="text/javascript" src="<?php  base_url()?>js/arcade/battle.js"></script>
+    </head> 
+    <body>  
+    	<h1>Login</h1>
+
+        <?php 
+        if (isset($errorMsg)) {
+    	    echo "<p>" . $errorMsg . "</p>";
         }
-	</style>
-	<script src="http://code.jquery.com/jquery-latest.js"></script>
-	<script type="text/javascript" src="<?php echo base_url();?>js/arcade/battle.js"></script>
-	<script>
-		var time;
-		var start_time;
-		var now_time;
-		var loginAttempts = <?= $_SESSION['loginAttempts']?>;
-		$(function() {
-			remaining = <?= $_SESSION['remaining'] ?>;
-			date = new Date();
-			start_time = Math.floor(date.getTime()/1000); // time since januar 1970? lol
-			delay = Math.pow(2,loginAttempts); // time is in seconds
-		});
+        echo form_open('account/login');
+        echo form_label('Username');
+        echo form_error('username');
+        echo form_input('username',set_value('username'),"required");
+        echo form_label('Password'); 
+        echo form_error('password');
+        echo form_password('password','',"required");
+    	echo form_submit('submit', 'Login');
+        echo "<p>" . anchor('account/newForm','Create Account') . "</p>";
+        echo "<p>" . anchor('account/recoverPasswordForm','Recover Password') . "</p>";
+        echo form_close();
+        ?>
 
-		function checktime() {
-			$(function() {
-				now = new Date();
-				now_time = Math.floor(now.getTime()/1000); 
-				var remaining = delay - (now_time - start_time); //recalculate time remaining
-				console.log(remaining);
-				var r = $('[name=remaining]'); // update time remaining
-				r.val(remaining);					
-				if (remaining > 0 && loginAttempts > 0){
-					//alert("You still have " + remaining + " seconds left before you can try again.");
-					return false;
-				} else if (remaining <= 0 && loginAttempts>=0){	
-					return false;
-				}
-				return false;
-			});
-		}
-	</script>
-</head> 
-<body>  
-	<h1>Login</h1>
+    	 <canvas id="myCanvas" width="200" height="200" />
 
-    <?php 
-    if (isset($errorMsg)) {
-	    echo "<p>" . $errorMsg . "</p>";
-    }
-    echo form_open('account/login');
-    echo form_label('Username');
-    echo form_error('username');
-    echo form_input('username',set_value('username'),"required");
-    echo form_label('Password'); 
-    echo form_error('password');
-    echo form_password('password','',"required");
-	echo form_submit('submit', 'Login', 'onClick="return checktime();"');
-    
-    echo form_hidden('remaining', set_value('remaining'));
-    
-    echo "<p>" . anchor('account/newForm','Create Account') . "</p>";
-    echo "<p>" . anchor('account/recoverPasswordForm','Recover Password') . "</p>";
-    echo form_close();
-    ?>
-
-    <canvas id="drawingCanvas" width="200" height="200"></canvas>
-</body>
+    </body>
 </html>
