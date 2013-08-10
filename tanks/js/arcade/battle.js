14bc63ded4b2ed557d73d60ba01b25963cbe3409
shichu.lin <shichu.lin@gmail.com>
2013-08-10 04:06:45 +0000

    refactored tank coordinates

--- tanks/js/arcade/battle.js
+++ tanks/js/arcade/battle.js
@@ -1,9 +1,6 @@
 var players = 2;
 var player1;
 var player2;
-var offsetX, offsetY; // used for moving tanks
-var mx = 0;
-var my = 0;
 var keys = {};
 var gameover = false;
 var id = 1; // for testing right now
@@ -87,7 +84,7 @@ function initTanks() {
 	context.clearRect(0, 0, canvasElement.width, canvasElement.height);
 
 	// draw tank positioned for player1
-	player1 = new Tank(0, 150, 50, 200, 0);
+	player1 = new Tank(0, 150, 50, 50, 0);
     player1.tankBody.fillColor = "beige";
     player1.tankBody.strokeColor = "blue";
     player1.turret.fillColor = "beige";
@@ -96,7 +93,7 @@ function initTanks() {
 	player1.draw();
 
 	// draw tank positioned for player2
-	player2 = new Tank(150, 0, 200, 50, 0);
+	player2 = new Tank(150, 0, 50, 50, 90);
     player2.tankBody.fillColor = "beige";
     player2.tankBody.strokeColor = "green";
     player2.turret.fillColor = "beige";
@@ -108,13 +105,12 @@ function initTanks() {
 /*
 	This is the Tank object. Its made up of a TankBody and a Turret
 */
-function Tank(x1, y1, x2, y2, angle) {
-    this.id = id;
-    this.tankBody = new TankBody(x1, y1, x2, y2);
-    var centerX = x1 + (x2 - x1) / 2;
-    var centerY = y1 + (y2 - y1) / 2;
-    this.turret = new Turret(centerX-8, centerY-35, centerX+8, centerY, angle);
-    this.cannon = new Cannon(centerX, centerY-8, x2, y2, angle);
+function Tank(x1, y1, w, h, angle) {
+    this.tankBody = new TankBody(x1, y1, w, h);
+    var centerX = x1 + w / 2;
+    var centerY = y1 + h / 2;
+    this.turret = new Turret(centerX-8.5, centerY-17, w/3, h/1.5, angle);
+    this.cannon = new Cannon(centerX, centerY, w, h, angle);
     this.draw = function(){
         this.tankBody.draw();
         this.turret.draw();
@@ -126,14 +122,16 @@ function Tank(x1, y1, x2, y2, angle) {
 	This is the TankBody object.
 	We define it's point of origin and then offset values with keyboard
 */
-function TankBody(x1, y1, x2, y2) {
-    this.update(x1, y1, x2, y2);
+function TankBody(x1, y1, w, h) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.w = w;
+    this.h = h,
 	this.outlineWidth = 3;
 	this.draw = function() {
 		// Draw the rectangle.
-		context.globalAlpha = 0.85;
 		context.beginPath();
-		context.rect(this.x1, this.y1, this.x2, this.y2);
+		context.rect(this.x1, this.y1, this.w, this.h);
 		context.fillStyle = this.fillColor;
 		context.strokeStyle = this.strokeColor;
 		context.fill();
@@ -141,7 +139,8 @@ function TankBody(x1, y1, x2, y2) {
 	};
 }
 
-TankBody.prototype.testHit = function(x, y) {
+//FIXME: has to be changed
+TankBody.prototype.testHit = function(x1, y1) {
 	var precision;
 	if (this.outlineWidth <= 5) {
 		var precision = this.outlineWidth + 2;
@@ -153,124 +152,80 @@ TankBody.prototype.testHit = function(x, y) {
 		var precision = this.outlineWidth /1.8;
 	} 
 
-	if (this.x2 < 0) {
-		if (this.y2 < 0) { // if box is drawn bottom right to top left
-			if (this.y2 < 0) { 
-				if (this.x1  + precision> x && this.x1+this.x2 - precision < x && this.y1 + precision > y && this.y1+this.y2 - precision < y) { 
+	if (this.w < 0) {
+		if (this.h < 0) { // if box is drawn bottom right to top left
+			if (this.h < 0) { 
+				if (this.x1  + precision> x1 && this.x1+this.w - precision < x1 && this.y1 + precision > y1 && this.y1+this.h - precision < y1) { 
 					return true;
 				}
 				return false;
 			}
-		} else { // x >0, box is drawn top right to bottom left
-			if (this.y2 > 0) { 
-				if (this.x1 +precision > x && this.x1+this.x2  - precision< x && this.y1  - precision< y && this.y1+this.y2 + precision > y) {
+		} else { // x1 >0, box is drawn top right to bottom left
+			if (this.h > 0) { 
+				if (this.x1 +precision > x1 && this.x1+this.w  - precision< x1 && this.y1  - precision< y1 && this.y1+this.h + precision > y1) {
 					return true;
 				}
 				return false;
 			}
 		}
-	} else { // x > 0
-		if (this.y2 < 0) { // box is drawn bottom left to top right
-			if (this.x1 - precision< x && this.x1+this.x2 +precision> x && this.y1  + precision> y && this.y1+this.y2 - precision< y) {
+	} else { // x1 > 0
+		if (this.h < 0) { // box is drawn bottom left to top right
+			if (this.x1 - precision< x1 && this.x1+this.w +precision> x1 && this.y1  + precision> y1 && this.y1+this.h - precision< y1) {
 				return true;
 			}
 			return false;
 		} else { // y > 0, box is drawn top left to bottom right 
-			if (this.x1 - precision < x && this.x1+this.x2  + precision> x && this.y1 - precision < y && this.y1+this.y2 + precision > y) {
+			if (this.x1 - precision < x1 && this.x1+this.w  + precision> x1 && this.y1 - precision < y1 && this.y1+this.h + precision > y1) {
 				return true;
 			}
 			return false;
 		}
 	}
-};
-
-TankBody.prototype.update = function(x1, y1, x2, y2) {
-	this.x1 = x1;
-	this.y1 = y1;
-	this.x2 = x2-this.x1; // offset value. distance between where tank begins and where mouse is
-	this.y2 = y2-this.y1;
 };
 
 /*
 	This is the Turret object
 	We define it's point of origin and then offset values with keyboard
 */
-function Turret(x1, y1, x2, y2, angle) {
-	this.update(x1, y1, x2, y2);
+function Turret(x1, y1, w, h, angle) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.w = w;
+    this.h = h,
+    //this.angle = angle;
+	this.angle = angle * Math.PI / 180;
 	this.outlineWidth = 2
-	this.isSelected = false;
+	console.log(this.x1);
+	console.log(this.y1);
+    console.log(this.w);
+	console.log(this.h);
+	console.log(this.angle);
 	this.draw = function() {
 		// Draw the turret
-		context.globalAlpha = 0.85;
+		context.save(); // saves the coordinate system
+		context.translate(this.x1 + this.w/2, this.y1 + this.h/2);
+        context.rotate(this.angle); // rotate around the start point
 		context.beginPath();
-		context.rect(this.x1, this.y1, this.x2, this.y2);
+		context.rect(-1*(this.w/2), -1*(this.h/2), this.w, this.h);
 		context.fillStyle = this.fillColor;
 		context.strokeStyle = this.strokeColor;
 		context.fill();
 		context.stroke();
+		context.restore(); // restores the coordinate system back to (0,0)
 	};
 }
-
-Turret.prototype.testHit = function(x,y) {
-	var precision;
-		if (this.outlineWidth <= 5) {
-		var precision = this.outlineWidth + 2;
-	} else if (this.outlineWidth <= 10) {
-		var precision = this.outlineWidth / 1.15;
-	} else if  (this.outlineWidth <= 15) {
-		var precision = this.outlineWidth / 1.5;
-	} else{
-		var precision = this.outlineWidth /1.8;
-	} 
-
-	if (this.x2 < 0) {
-		if (this.y2 < 0) { // if box is drawn bottom right to top left
-			if (this.y2 < 0) { 
-				if (this.x1  + precision> x && this.x1+this.x2 - precision < x && this.y1 + precision > y && this.y1+this.y2 - precision < y) { 
-					return true;
-				}
-				return false;
-			}
-		} else { // x >0, box is drawn top right to bottom left
-			if (this.y2 > 0) { 
-				if (this.x1 +precision > x && this.x1+this.x2  - precision< x && this.y1  - precision< y && this.y1+this.y2 + precision > y) {
-					return true;
-				}
-				return false;
-			}
-		}
-	} else { // x > 0
-		if (this.y2 < 0) { // box is drawn bottom left to top right
-			if (this.x1 - precision< x && this.x1+this.x2 +precision> x && this.y1  + precision> y && this.y1+this.y2 - precision< y) {
-				return true;
-			}
-			return false;
-		} else { // y > 0, box is drawn top left to bottom right 
-			if (this.x1 - precision < x && this.x1+this.x2  + precision> x && this.y1 - precision < y && this.y1+this.y2 + precision > y) {
-				return true;
-			}
-			return false;
-		}
-	}
-};
-
-Turret.prototype.update = function (x1, y1, x2, y2) {
-	this.x1 = x1;
-	this.y1 = y1;
-	this.x2 = x2-this.x1; // offset value. distance between where tank begins and where mouse is
-	this.y2 = y2-this.y1;
-};
 
 /*
 	This is the Cannon object
 	We define it's point of origin and then offset values with keyboard
 */
-function Cannon(x1, y1, x2, y2, angle) {
-	this.update(x1, y1, x2, y2);
+function Cannon(x1, y1, w, h, angle) {
+	this.x1 = x1;
+	this.y1 = y1;
+	this.radius = 5
 	this.outlineWidth = 2
 	this.draw = function() {
-		// Draw the circle.
-		context.globalAlpha = 0.85;
+		// Draw the cannon
 		context.beginPath();
 		context.arc(this.x1, this.y1, this.radius, 0, Math.PI*2);
 		context.fillStyle = this.fillColor;
@@ -279,24 +234,4 @@ function Cannon(x1, y1, x2, y2, angle) {
 		context.stroke();
 	};
 }
-
-Cannon.prototype.testHit = function(x, y) {
-	if (this.outlineWidth <= 5) {
-		var precision = this.outlineWidth;
-	} else if (this.outlineWidth <= 15) {
-		var precision = this.outlineWidth / 1.25;
-	} else {
-		var precision = this.outlineWidth / 1.75;
-	}
-	if (Math.sqrt(Math.pow(this.x1 - x, 2) + Math.pow(this.y1 -y, 2)) <= this.radius + precision) {
-		return true;
-	}
-	return false;
-};
-
-Cannon.prototype.update = function(x1, y1, x2, y2) {
-	this.x1 = x1;
-	this.y1 = y1;
-	this.radius = Math.sqrt(Math.pow(x2 - this.x1, 0.75) + Math.pow(y2-this.y1, 0.75));
-};
 
